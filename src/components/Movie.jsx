72c028d30c77ec50539bedaa2d3a1ea887ac8413
unknown <antipov.in@gmail.com>
2022-04-19 19:40:14 +0300

    class components were changed to functional ones

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,20 +1,35 @@
-function Movie (props) {
-    const {Title:title, Year:year, imdbID:id, Type:type, Poster:poster} = props;
+function Movie(props) {
+  const {
+    Title: title,
+    Year: year,
+    imdbID: id,
+    Type: type,
+    Poster: poster,
+  } = props;
 
-    return <div di ={id} className="card movie">
+  return (
+    <div di={id} className="card movie">
       <div className="card-image waves-effect waves-block waves-light">
-          {
-            poster === 'N/A' ?
-            <img className="activator" src={`https://via.placeholder.com/300x400?text=${title}`}/>:
-            <img className="activator" src={poster}/>
-          }
-        
+        {poster === 'N/A' ? (
+          <img
+            className="activator"
+            src={`https://via.placeholder.com/300x400?text=${title}`}
+            alt="ups"
+          />
+        ) : (
+          <img className="activator" src={poster} alt="ups" />
+        )}
       </div>
       <div className="card-content">
-        <span className="card-title activator grey-text text-darken-4">{title}</span>
-        <p>{year} <span className="right">{type}</span></p>
+        <span className="card-title activator grey-text text-darken-4">
+          {title}
+        </span>
+        <p>
+          {year} <span className="right">{type}</span>
+        </p>
       </div>
     </div>
+  );
 }
 
-export {Movie}
+export { Movie };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,85 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Search extends React.Component {
-  state = {
-    search: '',
-    type: 'all',
-  };
+const Search = (props) => {
+  const { search_movies = Function.prototype } = props;
+  const [search, setSearch] = useState('');
+  const [type, setType] = useState('all');
 
-  handleKey = (event) => {
+  const handleKey = (event) => {
     if (event.key === 'Enter') {
-      this.props.search_movies(this.state.search, this.state.type);
+      search_movies(search, type);
     }
   };
 
-  handle_filter = (event) => {
-    this.setState(
-      () => ({ type: event.target.dataset.type }),
-      () => {
-        this.props.search_movies(this.state.search, this.state.type);
-      }
-    );
+  const handle_filter = (event) => {
+    setType(event.target.dataset.type);
+    search_movies(search, event.target.dataset.type);
   };
-  render() {
-    return (
-      <div className="row">
-        <div className="input-field">
-          <input
-            placeholder="Search"
-            type="search"
-            id="email_inline"
-            className="validate"
-            value={this.state.search}
-            onChange={(event) => this.setState({ search: event.target.value })}
-            onKeyDown={this.handleKey}
-          />
-          <button
-            className="btn search-btn"
-            onClick={() =>
-              this.props.search_movies(this.state.search, this.state.type)
-            }
-          >
-            Search
-          </button>
-        </div>
-
-        <p>
-          <label>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="all"
-              checked={this.state.type === 'all'}
-              onChange={this.handle_filter}
-            />
-            <span>All</span>
-          </label>
-          <label>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="movie"
-              onChange={this.handle_filter}
-              checked={this.state.type === 'movie'}
-            />
-            <span>Movies</span>
-          </label>
-          <label>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="series"
-              onChange={this.handle_filter}
-              checked={this.state.type === 'series'}
-            />
-            <span>Series</span>
-          </label>
-        </p>
+  return (
+    <div className="row">
+      <div className="input-field">
+        <input
+          placeholder="Search"
+          type="search"
+          id="email_inline"
+          className="validate"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          onKeyDown={handleKey}
+        />
+        <button
+          className="btn search-btn"
+          onClick={() => search_movies(search, type)}
+        >
+          Search
+        </button>
       </div>
-    );
-  }
-}
+
+      <p>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="all"
+            checked={type === 'all'}
+            onChange={handle_filter}
+          />
+          <span>All</span>
+        </label>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="movie"
+            onChange={handle_filter}
+            checked={type === 'movie'}
+          />
+          <span>Movies</span>
+        </label>
+        <label>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="series"
+            onChange={handle_filter}
+            checked={type === 'series'}
+          />
+          <span>Series</span>
+        </label>
+      </p>
+    </div>
+  );
+};
 export { Search };
